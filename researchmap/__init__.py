8e8919bd474f38c84f8bca38ae3bbfae45bf36af
RTa-technology <scpfoundationidcardmaker@gmail.com>
2022-02-18 17:16:57 +0900

    feat: add API Wrapper

---
+++ researchmap/__init__.py
@@ -0,0 +1,28 @@
+"""
+Researchmap API wrapper
+
+This is a wrapper for the Researchmap API. It provides a simple interface to
+make requests to the API and return the results as a dictionary.
+
+The API documentation can be found at https://researchmap.org/api/
+:copyright: (c) 2022 by the authors and contributors (see AUTHORS).
+:license: MIT, see LICENSE for more details.
+"""
+__title__ = 'researchmap'
+__author__ = 'RTa_technology'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2022 by the authors and contributors (see AUTHORS)'
+__version__ = '0.1.0'
+
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
+from .core import Adapter, AiohttpAdapter, RequestsAdapter
+from .adapter import Wrapper
+
+__all__ = [
+  'Wrapper',
+  'Adapter',
+  'AiohttpAdapter',
+  'RequestsAdapter'
+]
+
