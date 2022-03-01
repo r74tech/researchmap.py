d01b13e3c58b131321921dbdb8579903495881ab
RTa-technology <scpfoundationidcardmaker@gmail.com>
2022-03-01 12:13:00 +0900

    fix: theme

--- docs/conf.py
+++ docs/conf.py
@@ -64,7 +64,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-autodoc_mock_imports = ["jwt", "cryptography"]
+autodoc_mock_imports = ["jwt", "cryptography","requests","aiohttp"]
 
 
 # -- Extension configuration -------------------------------------------------
